4523db461b6a7b718b05db26585bd63d201e58f0
Hampus Mattsson <hjm@ownit.nu>
2024-12-01 10:52:12 +0100

    Day1 Part1

---
+++ AdventOfCode2024/AdventOfCode2024/Day1/Day1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace AdventOfCode2024.Day1
+{
+    public class Day1
+    {
+        public void Main()
+        {
+            var Day1Path = Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\Day1\Example.txt");
+            //var Day1Path = Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\PuzzleInputs\Day1.txt");
+
+            var input = File.ReadAllLines(Day1Path);
+
+            Part2(input);
+        }
+
+        int Part2(string[]? input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                Console.WriteLine(input[i]);
+            }
+
+            return 0;
+        }
+
+
+        internal int Part1(string[]? input)
+        {
+            var leftSideTotal = input.Select(x => { return Int32.Parse(x.Split("   ")[0]); }).OrderBy(x => x).ToArray();
+            var rightSideTotal = input.Select(x => { return Int32.Parse(x.Split("   ")[1]); }).OrderBy(x => x).ToArray();
+
+            int totalDiff = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                var left = leftSideTotal[i];
+                var right = rightSideTotal[i];
+
+                int diff = left - right;
+
+                if (diff < 0)
+                {
+                    diff = diff * -1;
+                }
+
+                totalDiff += diff;
+            }
+
+            Console.WriteLine("TOTAL DIFF: " + totalDiff);
+            return totalDiff;
+        }
+    }
+}
---
+++ AdventOfCode2024/AdventOfCode2024/Program.cs
@@ -0,0 +1,7 @@
+﻿using AdventOfCode2024.Day1;
+
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+Day1 Day1 = new();
+Day1.Main();
