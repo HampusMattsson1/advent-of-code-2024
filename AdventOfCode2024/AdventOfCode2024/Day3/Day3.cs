f53385a53fbd507ab0caa2cf4352d8e72de91b99
Mattsson <hampus.mattsson@regionblekinge.se>
2024-12-03 09:28:38 +0100

    Day 3 idé

---
+++ AdventOfCode2024/AdventOfCode2024/Day3/Day3.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using static System.Net.Mime.MediaTypeNames;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2024.Day3
+{
+    public class Day3
+    {
+        public void Main()
+        {
+            var DayPath = Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", "Day3", "Example.txt");
+            // var DayPath = Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", "PuzzleInputs", "Day2.txt");
+
+            var input = File.ReadAllLines(DayPath);
+
+            Console.WriteLine("DAY 3");
+
+            Part1(input);
+            //Part2(input);
+        }
+
+
+
+        internal int Part1(string[]? input)
+        {
+            var regex = new Regex("xmul\\(\\d,\\d\\)", RegexOptions.IgnoreCase);
+
+            foreach(var line in input)
+            {
+                var matches = regex.Matches(line);
+
+                foreach (Match match in matches)
+                {
+                    Console.WriteLine("MATCH, " + match.ToString());
+                }
+            }
+
+            return 0;
+        }
+
+        
+    }
+}
--- AdventOfCode2024/AdventOfCode2024/Program.cs
+++ AdventOfCode2024/AdventOfCode2024/Program.cs
@@ -1,5 +1,6 @@
 ﻿using AdventOfCode2024.Day1;
 using AdventOfCode2024.Day2;
+using AdventOfCode2024.Day3;
 
 // See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
@@ -8,4 +9,7 @@ using AdventOfCode2024.Day2;
 //Day1.Main();
 
 Day2 Day2 = new();
-Day2.Main();
+//Day2.Main();
+
+Day3 Day3 = new();
+Day3.Main();
