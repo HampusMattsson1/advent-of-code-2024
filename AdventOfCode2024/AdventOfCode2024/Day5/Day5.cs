f156effad6f676d752a4439e5066b3ea1f4e1415
Mattsson <hampus.mattsson@regionblekinge.se>
2024-12-05 15:00:31 +0100

    Initial day5 idé

---
+++ AdventOfCode2024/AdventOfCode2024/Day5/Day5.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using static System.Net.Mime.MediaTypeNames;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2024.Day5
+{
+    public class Day5
+    {
+        public void Main()
+        {
+            var DayPath = Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", "Day5", "Example.txt");
+            //var DayPath = Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", "PuzzleInputs", "Day5.txt");
+
+            var input = File.ReadAllLines(DayPath);
+
+            Console.WriteLine("DAY 5");
+
+            Part1(input);
+            //Part2(input);
+        }
+
+
+        void Part1(string[] rows)
+        {
+            var tableRows = rows.TakeWhile(r => r != "").ToList();
+
+            var inputRows = rows.Where((e, i) => i > tableRows.Count).ToList();
+
+            foreach (var row in inputRows)
+            {
+                var pages = row.Split(',');
+
+                foreach (var page in pages)
+                {
+
+                }
+            }
+        }
+
+    }
+}
--- AdventOfCode2024/AdventOfCode2024/Program.cs
+++ AdventOfCode2024/AdventOfCode2024/Program.cs
@@ -2,6 +2,7 @@
 using AdventOfCode2024.Day2;
 using AdventOfCode2024.Day3;
 using AdventOfCode2024.Day4;
+using AdventOfCode2024.Day5;
 
 // See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
@@ -9,11 +10,14 @@ using AdventOfCode2024.Day4;
 //Day1 Day1 = new();
 //Day1.Main();
 
-Day2 Day2 = new();
-Day2.Main();
+//Day2 Day2 = new();
+//Day2.Main();
 
 //Day3 Day3 = new();
 //Day3.Main();
 
 //Day4 Day4 = new();
 //Day4.Main();
+
+Day5 Day5 = new();
+Day5.Main();
