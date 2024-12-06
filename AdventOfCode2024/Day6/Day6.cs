50d72003d4b89a9c26bc673ac0170b9766309a55
Mattsson <hampus.mattsson@regionblekinge.se>
2024-12-06 16:21:20 +0100

    Day 6 init

---
+++ AdventOfCode2024/Day6/Day6.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using static System.Net.Mime.MediaTypeNames;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2024.Day6
+{
+    public class Day6
+    {
+        public void Main()
+        {
+            var DayPath = Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", "Day6", "Example.txt");
+            //var DayPath = Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", "PuzzleInputs", "Day6.txt");
+
+            var input = File.ReadAllLines(DayPath);
+
+            Console.WriteLine("DAY 6");
+
+            Part1(input);
+            //Part2(input);
+        }
+
+
+        void Part1(string[] rows)
+        {
+            
+        }
+
+    }
+}
